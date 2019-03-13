aa90029bdc8ceaffcee7bee3f521cc9e7a007b32
WhiteSummeRK <kauan.alves1996@hotmail.com>
2019-03-13 13:54:28 -0300

    testes de deploy 02

--- Procfile
+++ Procfile
@@ -1,5 +1,2 @@
 web:
-  python migrations.py db init
-  python migrations.py db migrate
-  python migrations.py db upgrade
-  python manage.py runserver
+  ./start.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 export FLASK_APP=manticora
 export FLASK_DEBUG=1
-flask run --host=0.0.0.0
+
+python migrations.py db init
+python migrations.py db migrate
+python migrations.py db upgrade
+python manage.py runserver
