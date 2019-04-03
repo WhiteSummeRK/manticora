e542f93ada9e9cb2a6df37ad83070545a93e7b01
WhiteSummeRK <kauan.alves1996@hotmail.com>
2019-04-03 20:12:19 -0300

    pré deploy

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ manager = Manager(app)
 @manager.command
 def runserver():
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
